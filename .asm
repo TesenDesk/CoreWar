42768b38c1f340de38a4fb27da7bbe9e321fcd4d
mstygg <mstygg@student.21-school.ru>
2020-02-03 17:40:05 +0300

    lexer_model_almost_done

--- .asm
+++ .asm
@@ -5,12 +5,14 @@
  <group name="settings">
   <item name="tabs">1</item>
   <item name="windows">0</item>
-  <item name="grid">2</item>
+  <item name="grid">3</item>
   <item name="backups">0</item>
  </group>
  <group name="windows">
-  <item id="asm::Class1::SM">0,0,640,480</item>
-  <item id="asm::Class2::SM">0,0,1899,1123,*</item>
+  <item id="asm::Class1::SM">0,0,1094,643</item>
+  <item id="asm::asm::Class1::SM">0,0,640,480</item>
+  <item id="asm::asm::Class2::SM">0,0,1948,998,*</item>
+  <item id="asm::asm::Class2::SM::argument_unit">1094,480,640,480</item>
  </group>
  <group name="search">
   <item name="options">2032128</item>
