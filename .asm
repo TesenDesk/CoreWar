644884844fe52c1d97e2c3c749ac83f33ac00a36
ftothmur <ftothmur@student.21-school.ru>
2020-02-04 14:57:19 +0300

    Поправил диаграмму чемпиона приведя в соотвествие с диаграммой 1го уровня.

--- .asm
+++ .asm
@@ -9,10 +9,8 @@
   <item name="backups">0</item>
  </group>
  <group name="windows">
-  <item id="asm::Class1::SM">0,0,1094,643</item>
-  <item id="asm::asm::Class1::SM">0,0,640,480</item>
-  <item id="asm::asm::Class2::SM">0,0,1948,998,*</item>
-  <item id="asm::asm::Class2::SM::argument_unit">1094,480,640,480</item>
+  <item id="asm::Class1::SM">0,0,640,480</item>
+  <item id="asm::asm::Class2::SM">0,0,1432,1001,*</item>
  </group>
  <group name="search">
   <item name="options">2032128</item>
