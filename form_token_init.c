ce424796aad6ebaff0fa22567bf6f7ab6188c8aa
mstygg <mstygg@student.21-school.ru>
2020-02-05 00:19:46 +0300

    switcher_naming_added

--- form_token_init.c
+++ form_token_init.c
@@ -30,14 +30,17 @@ void                construct_lexer(t_lexer **lexer);
 void                construct_token(t_token *token);
 
 void                form_token(t_lexer  *lexer, )
-void                get_term(void * (*fptr_get_term_ar)[7]);
+void                get_term(char **str, void  (*fptr_get_term_ar)[3]);
 /*
  * init_state_terms
  */
+void                (*fptr_init_term_switch)(char** str, void (*fptr)[4]);
 void                get_term_init_state(char**str);
 /*
  * 1lvl token substates
  */
+
+void                (*fptr_1sub_term_switch)(char** str,(void) (*fptr)[2] );
 void                get_term_comment_unit(char**str);
 void                get_term_line_feed_unit(char**str);
 void                get_term_champion_unit(char**str);
@@ -45,12 +48,14 @@ void                get_term_code_unit(char**str);
 /*
  * code_state_terms
  */
+void                (*fptr_code_state_term_switch)(char** str, (void) (*fptr)[2]);
 void                get_term_op_name_unit(char**str);
 void                get_term_label_word_unit(char**str);
 /*
  * op_name_terms
  */
 void                get_term_opx_name(char**str);
+void                get_term_argument_unit(char**str);
 
 
 
