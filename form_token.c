09964c38557532bf06ff1f9373fe551389b7790a
mstygg <mstygg@student.21-school.ru>
2020-02-05 17:06:45 +0300

    form_token_init

--- form_token.c
+++ form_token.c
@@ -1,7 +1,7 @@
 //
 // Created by Mudcrab Stygg on 04/02/2020.
 //
-
+#include "form_token.h"
 //typedef textuct 	    s_lexer
 //{
 //    int             state;
@@ -9,22 +9,25 @@
 //    void            (*fptr_form_token)(textuct s_lexer *lexer, char **text);
 //}				    t_lexer;
 
-#include <stdio.h>
-#include <stdlib.h>
+
+typedef struct 	    s_lexer
+{
+    int             state;
+    t_token*        (*form_token)(textuct s_lexer *lexer, char **text);
+}				    t_lexer;
 
 
-typedef             struct s_lexer t_lexer;
-typedef             struct s_token t_token;
+typedef struct 	    s_token
+{
+    int             tkn_type;
+    void		    *tkn_begin;
+    size_t		    tkn_len;
+}				    t_token;
 
-void                lexer_constructor(t_lexer **lexer);
-void                token_constructor(t_token **token);
 
-void                form_token(t_lexer  *lexer, char **text);
 
-/*
- * get_term_inner_methods
- */
-void                get_term(char **text, void  (*fptr_get_term_ar)(char**)[3]);
+t_token*            form_token(t_lexer  *lexer, char **text);
+//void                get_term(char **text, void  (*fptr_get_term_ar)(char**)[3]);
 /*
  * init_state_terms
  */
@@ -49,5 +52,34 @@ void                get_term_label_word_unit(char **text);
  * op_name_terms
  */
 void                get_term_opx_name(char **text);
-void                get_term_argument_unit(char **text);
+void                get_label_word_unitt (char **text);
+
+
+
+
+void                lexer_constructor(t_lexer **lexer)
+{
+    if (!(*lexer = (t_lexer*)malloc(sizeof(t_lexer))))
+    {
+    	printf("error\n");
+    	exit(-1);
+    }
+	(*lexer)->state = 0;
+	(*lexer)->form_token = form_token;
+}
+
+void                token_constructor(t_token **token)
+{
+	if (!(*token = (t_token*)malloc(sizeof(t_token))))
+	{
+		printf("error\n");
+		exit(-1);
+	}
+	(*token)->tkn_type = 0;
+	(*token)->tkn_begin = NULL;
+	(*token)->tkn_end = NULL;
+	(*token)->tkn_len = 0;
+}
+
+
 
