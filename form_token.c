c6aa3c348c6cc45745d708316fa7c253b25b38a1
mstygg <mstygg@student.21-school.ru>
2020-02-05 19:08:07 +0300

    get_terms_now_token_methods

--- form_token.c
+++ form_token.c
@@ -27,32 +27,32 @@ typedef struct 	    s_token
 
 
 t_token*            form_token(t_lexer  *lexer, char **text);
-//void                get_term(char **text, void  (*fptr_get_term_ar)(char**)[3]);
+//void                tkn_get_term(char **text, void  (*fptr_tkn_get_term_ar)(char**)[3]);
 /*
  * init_state_terms
  */
-void                (*fptr_init_term_switch)(char **text, void (*fptr)(char**)[4]);
-void                get_term_init_state(char**text);
+void                (*fptr_init_term_switch)(t_token *token, char **text, void (*fptr)(t_token *token,char**)[4]);
+void                tkn_get_term_init_state(t_token *token, char**text);
 /*
  * 1lvl token substates
  */
 
-void                (*fptr_1sub_term_switch)(char **text,(void) (*fptr)(char**)[2] );
-void                get_term_comment_unit(char **text);
-void                get_term_line_feed_unit(char **text);
-void                get_term_champion_unit(char **text);
-void                get_term_code_unit(char **text);
+void                (*fptr_1sub_term_switch)(t_token *token, char **text,(void) (*fptr)(t_token *token, char**)[2] );
+void                tkn_get_term_comment_unit(t_token *token, char **text);
+void                tkn_get_term_line_feed_unit(t_token *token, char **text);
+void                tkn_get_term_champion_unit(t_token *token, char **text);
+void                tkn_get_term_code_unit(t_token *token, char **text);
 /*
  * code_state_terms
  */
-void                (*fptr_code_state_term_switch)(char **text, (void) (*fptr)(char**)[2]);
-void                get_term_op_name_unit(char **text);
-void                get_term_label_word_unit(char **text);
+void                (*fptr_code_state_term_switch)(t_token *token, char **text, (void) (*fptr)(char**)[2]);
+void                tkn_get_term_op_name_unit(t_token *token, char **text);
+void                tkn_get_term_label_word_unit(t_token *token, char **text);
 /*
  * op_name_terms
  */
-void                get_term_opx_name(char **text);
-void                get_label_word_unitt (char **text);
+void                tkn_get_term_opx_name(t_token *token, char **text);
+void                tkn_get_label_word_unitt (t_token *token, char **text);
 
 
 
--- form_token.h
+++ form_token.h
@@ -16,38 +16,32 @@
 typedef             struct s_lexer t_lexer;
 typedef             struct s_token t_token;
 
-void                lexer_constructor(t_lexer **lexer);
-void                token_constructor(t_token **token);
-
-void                form_token(t_lexer  *lexer, char **text);
-
-/*
- * get_term_inner_methods
- */
-void                get_term(char **text, void  (*fptr_get_term_ar)(char**)[3]);
+t_token*            form_token(t_lexer  *lexer, char **text);
 /*
  * init_state_terms
  */
-void                (*fptr_init_term_switch)(char **text, void (*fptr)(char**)[4]);
-void                get_term_init_state(char**text);
+void                (*fptr_init_term_switch)(t_token *token, char **text, void (*fptr)(t_token *token,char**)[4]);
+void                tkn_get_term_init_state(t_token *token, char**text);
 /*
  * 1lvl token substates
  */
 
-void                (*fptr_1sub_term_switch)(char **text,(void) (*fptr)(char**)[2] );
-void                get_term_comment_unit(char **text);
-void                get_term_line_feed_unit(char **text);
-void                get_term_champion_unit(char **text);
-void                get_term_code_unit(char **text);
+void                (*fptr_1sub_term_switch)(t_token *token, char **text,(void) (*fptr)(t_token *token, char**)[2] );
+void                tkn_get_term_comment_unit(t_token *token, char **text);
+void                tkn_get_term_line_feed_unit(t_token *token, char **text);
+void                tkn_get_term_champion_unit(t_token *token, char **text);
+void                tkn_get_term_code_unit(t_token *token, char **text);
 /*
  * code_state_terms
  */
-void                (*fptr_code_state_term_switch)(char **text, (void) (*fptr)(char**)[2]);
-void                get_term_op_name_unit(char **text);
-void                get_term_label_word_unit(char **text);
+void                (*fptr_code_state_term_switch)(t_token *token, char **text, (void) (*fptr)(char**)[2]);
+void                tkn_get_term_op_name_unit(t_token *token, char **text);
+void                tkn_get_term_label_word_unit(t_token *token, char **text);
 /*
  * op_name_terms
  */
-void                get_term_opx_name(char **text);
-void                get_term_argument_unit(char **text);
+void                tkn_get_term_opx_name(t_token *token, char **text);
+void                tkn_get_label_word_unitt (t_token *token, char **text);
+
+
 
