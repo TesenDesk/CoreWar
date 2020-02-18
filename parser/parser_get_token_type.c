ac1702fecf159478ca37ec802c342ee6890eb027
Clement Missy <cmissy@at-a1.21-school.ru>
2020-02-18 17:03:05 +0300

    start make pareser_get_token_type

---
+++ parser/parser_get_token_type.c
@@ -0,0 +1,13 @@
+#include "pareser_private.h"
+#include "token_defines.h"
+
+int			parser_get_token_type(t_parser *parser, int token_type)
+{
+	// init
+	if (pareser->state == INIT)
+	{
+		if (token_type == TOKEN_CHNAME || token_type == TOKEN_CHCOM)
+		esle if (token_type == TOKEN_LABEL_WORD)
+		else if (token_type == TOKEN_LFEED)
+	}
+}
