f57e2f43a2e0defc60a5906b9544b73114ad5c62
Clement Missy <cmissy@at-a1.21-school.ru>
2020-02-18 19:23:52 +0300

    bla

--- parser/parser_get_token_init.c
+++ parser/parser_get_token_init.c
@@ -1,7 +1,18 @@
+#include "parser_private.h"
 
-
-int				parser_get_token_init(t_pareser *parser, ...)
+int				parser_get_token_init(struct s_parser *parser, t_lexer *lexer, char const **text)
 {
 	int			token_type;
-	token_type = get_token_type(lexer_form_token(lex, &text));
+
+	token_type = get_token_type(lexer_form_token(lexer, text));
+	if (token_type == TOKEN_CHNAME || token_type == TOKEN_CHCOM)
+		return ();
+	else if (token_type == TOKEN_OPX)
+		return ();
+	else if (token_type == TOKEN_LFEED)
+		return ();
+	else if (token_type == TOKEN_LAWO) /// WHERE???
+		return ();
+	else if (token_type == TOKEN_LAWO) /// WHERE???
+		return ();
 }
