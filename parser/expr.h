33ca6c092b347c06b74a5f6200506d10d8af42f7
Clement Missy <cmissy@at-e2.21-school.ru>
2020-02-18 20:46:39 +0300

    better

--- parser/expr.h
+++ parser/expr.h
@@ -26,7 +26,6 @@ typedef struct 	    s_expr t_expr;
  * construction/destruction
 */
 
-
 t_expr*            expr_constructor(int expr_type,void *expr_ptr[2]);
 void               expr_destructor(t_expr **expr);
 
