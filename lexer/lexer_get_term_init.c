652701528d7fe778cae3bf65f2e200ccc11aeca2
mstygg <mstygg@student.21-school.ru>
2020-02-19 18:56:04 +0300

    make and main chenged

--- lexer/lexer_get_term_init.c
+++ lexer/lexer_get_term_init.c
@@ -1,5 +1,4 @@
-
-#include "lexer.h"
+#include "token_defines.h"
 #include "lexer_private.h"
 
 static int      term_is_op(char const *text)
@@ -19,6 +18,26 @@ static int      term_is_op(char const *text)
     	return (5);
     return (0);
 }
+static int      find_op_code(char const *text)
+{
+	if (strnstr(text, LD_NAME, 2))
+		return (LD_NAME_CODE);
+		else if (strnstr(text, ST_NAME, 2))
+//		return (2);
+//	else if (strnstr(text, ADD_NAME,3) || strnstr(text, AFF_NAME, 3)
+//	         || strnstr(text, AND_NAME, 3) || strnstr(text, LDI_NAME, 3)
+//	         || strnstr(text, LLD_NAME, 3) || strnstr(text, STI_NAME, 3)
+//	         || strnstr(text, SUB_NAME, 3) || strnstr(text, XOR_NAME, 3))
+//		return (3);
+//	else if (strnstr(text, FORK_NAME, 4) || strnstr(text, LIVE_NAME, 4)
+//	         || strnstr(text, LLDI_NAME, 4) || strnstr(text, ZJMP_NAME, 4))
+//		return (4);
+//	else if (strnstr(text, LFORK_NAME, 5))
+//		return (5);
+//	return (0);
+
+}
+
 
 int             lexer_get_term_init(t_lexer *lexer, char const **text, int *type, void *token_ptr[2])
 {
