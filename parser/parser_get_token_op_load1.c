caaf31cd3d98984589a603c1cfc04edf942afa34
ftothmur <ftothmur@student.21-school.ru>
2020-02-21 15:44:20 +0300

    load1 implemented.

---
+++ parser/parser_get_token_op_load1.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_get_token_op_load1.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 15:43:04 by ftothmur          #+#    #+#             */
+/*   Updated: 2020/02/21 15:43:52 by ftothmur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expr.h"
+#include "expr_defines.h"
+#include "expr_private.h"
+#include "parser_private.h"
+
+int				parser_get_token_op_load1(t_parser *parser, t_lexer *lexer,
+				t_expr *expr, char const **text)
+{
+	int		token_type;
+
+	token_type = get_type(lexer_form_token(lexer, text));
+	if (!(token_type == TOKEN_TREG))
+		expr->token = EXPR_UNDEF;
+	return (token_type);
+}
