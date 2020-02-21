816366647945f9a6612683d046a9d78ed8ff9f10
Clement Missy <cmissy@at-e6.21-school.ru>
2020-02-21 15:52:53 +0300

    parser_get_term_init ready

--- parser/parser_get_token_init.c
+++ parser/parser_get_token_init.c
@@ -16,27 +16,26 @@ int			parser_get_token_init(t_parser *parser, t_lexer *lexer,
 		expr->token = EXPR_CH_COMMENT_LINE;
 	else if (token_type == TOKEN_LFEED)
 		expr->token = EXPR_PRECODE_COMMENT_LINE;
-	else if (token_type <= TOKEN_ADD)
-		expr->token = EXPR_PRECODE_COMMENT_LINE;
-	else if (token_type <= TOKEN_AFF)
-	else if (token_type <= TOKEN_AND)
-	else if (token_type <= TOKEN_FORK)
-	else if (token_type <= TOKEN_LD)
-		expr->token = EXPR_OP_LOAD;
-	else if (token_type <= TOKEN_LDI)
-	else if (token_type <= TOKEN_LFORK)
-	else if (token_type <= TOKEN_LIVE)
-	else if (token_type <= TOKEN_LLD)
-	else if (token_type <= TOKEN_LLDI)
-	else if (token_type <= TOKEN_OR)
-	else if (token_type <= TOKEN_ST)
-		expr->token = EXPR_OP_STOR;
-	else if (token_type <= TOKEN_STI)
-	else if (token_type <= TOKEN_SUB)
-	else if (token_type <= TOKEN_XOR)
-	else if (token_type <= TOKEN_ZJMP)
 	else if (token_type == TOKEN_LAB_WO)
 		expr->token = EXPR_CODE_COMMENT_LINE;
+
+	else if (token_type == TOKEN_LIVE || token_type == TOKEN_ZJMP || token_type == TOKEN_FORK || token_type == TOKEN_LFORK)
+		expr->token = EXPR_OP_LIFE;
+	else if (token_type == TOKEN_AFF)
+		expr->token = EXPR_OP_AFCT;
+	else if (token_type == TOKEN_LD || token_type == TOKEN_LLD)
+		expr->token = EXPR_OP_LOAD;
+	else if (token_type == TOKEN_ST)
+		expr->token = EXPR_OP_STOR;
+	else if (token_type == TOKEN_ADD || token_type == TOKEN_SUB)
+		expr->token = EXPR_OP_ARIT;
+	else if (token_type == TOKEN_LDI || token_type == TOKEN_LLDI)
+		expr->token = EXPR_OP_LODI;
+	else if (token_type == TOKEN_AND || token_type == TOKEN_OR || token_type == TOKEN_XOR)
+		expr->token = EXPR_OP_LOGC;
+	else if (token_type == TOKEN_STI)
+		expr->token = EXPR_OP_STRI;
+
 	else
 		expr->token = EXPR_UNDEF;
 	return (token_type);
