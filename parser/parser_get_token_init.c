4e8135bfc3e70a0d4d3c84bc1f921325ad6c4fe9
Clement Missy <cmissy@at-d6.21-school.ru>
2020-02-23 17:41:49 +0300

    ops

--- parser/parser_get_token_init.c
+++ parser/parser_get_token_init.c
@@ -11,32 +11,34 @@ int			parser_get_token_init(t_parser *parser, t_lexer *lexer,
 	expr->begin = *text;
 	token_type = get_type(lexer_form_token(lexer, text));
 	if (token_type == TOKEN_CHNAME)
-		expr->token = EXPR_CH_NAME_LINE;
+		expr->type = EXPR_CH_NAME_LINE;
 	else if (token_type == TOKEN_CHCOM)
-		expr->token = EXPR_CH_COMMENT_LINE;
+		expr->type = EXPR_CH_COMMENT_LINE;
 	else if (token_type == TOKEN_LFEED)
-		expr->token = EXPR_PRECODE_COMMENT_LINE;
+		expr->type = EXPR_PRECODE_COMMENT_LINE;
 	else if (token_type == TOKEN_LAB_WO)
-		expr->token = EXPR_CODE_COMMENT_LINE;
+		expr->type = EXPR_CODE_COMMENT_LINE;
 
-	else if (token_type == TOKEN_LIVE || token_type == TOKEN_ZJMP || token_type == TOKEN_FORK || token_type == TOKEN_LFORK)
-		expr->token = EXPR_OP_LIFE;
+	else if (token_type == TOKEN_LIVE || token_type == TOKEN_ZJMP ||
+	token_type == TOKEN_FORK || token_type == TOKEN_LFORK)
+		expr->type = EXPR_OP_LIFE;
 	else if (token_type == TOKEN_AFF)
-		expr->token = EXPR_OP_AFCT;
+		expr->type = EXPR_OP_AFCT;
 	else if (token_type == TOKEN_LD || token_type == TOKEN_LLD)
-		expr->token = EXPR_OP_LOAD;
+		expr->type = EXPR_OP_LOAD;
 	else if (token_type == TOKEN_ST)
-		expr->token = EXPR_OP_STOR;
+		expr->type = EXPR_OP_STOR;
 	else if (token_type == TOKEN_ADD || token_type == TOKEN_SUB)
-		expr->token = EXPR_OP_ARIT;
+		expr->type = EXPR_OP_ARIT;
 	else if (token_type == TOKEN_LDI || token_type == TOKEN_LLDI)
-		expr->token = EXPR_OP_LODI;
-	else if (token_type == TOKEN_AND || token_type == TOKEN_OR || token_type == TOKEN_XOR)
-		expr->token = EXPR_OP_LOGC;
+		expr->type = EXPR_OP_LODI;
+	else if (token_type == TOKEN_AND || token_type == TOKEN_OR ||
+	token_type == TOKEN_XOR)
+		expr->type = EXPR_OP_LOGC;
 	else if (token_type == TOKEN_STI)
-		expr->token = EXPR_OP_STRI;
+		expr->type = EXPR_OP_STRI;
 
 	else
-		expr->token = EXPR_UNDEF;
+		expr->type = EXPR_UNDEF;
 	return (token_type);
 }
