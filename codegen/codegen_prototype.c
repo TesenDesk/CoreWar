ed9b784d87f2c19a7698999d8affbf07a5dfda8b
jjerde <jjerde@student.21-school.ru>
2020-02-26 21:35:14 +0300

    Some code added............

--- codegen/codegen_prototype.c
+++ codegen/codegen_prototype.c
@@ -8,6 +8,7 @@ struct s_codegen
 {
 	int stage;
 	int add;
+	char
 	struct s_labelmap *labels_free;
 	struct s_labelmap *labels_queue;
 };
@@ -22,11 +23,13 @@ void set_label( t_codegen *code, sequenced_text *seq)
 		add_address(code->labels_queue, code->add);
 }
 
-t_codegen *codegen_constructor()
+t_codegen *codegen_constructor(struct s_labelmap *labels_free, struct s_labelmap *labels_queue)
 {
 	t_codegen *code;
 
 	code = ft_memalloc(sizeof(t_codegen));
+	code->labels_free = labels_free;
+	code->labels_queue = labels_queue;
 	return (code);
 }
 
@@ -41,6 +44,52 @@ void codegen_destructor(t_codegen *code)
 		free(code);
 		code = NULL;
 	}
+}
+
+char *codegen_generate_magic(void)
+{
+	char *magic;
+	size_t size;
+
+	size = sizeof(COREWAR_EXEC_MAGIC);
+	if (!(magic = ft_memalloc(size)))
+	{
+		return (NULL);
+	}
+	ft_memcpy(magic, COREWAR_EXEC_MAGIC, size);
+	return (magic);
+}
+
+char *codegen_generate_champ_name_null(t_sequenced_text *champ_name)
+{
+	char *name;
+
+	if (!(name = ft_memalloc(PROG_NAME_LENGTH + 4)))
+		return (NULL);
+	ft_memcpy(name, champ_name->content, sizeof(champ_name->content));
+	return (name);
+}
+
+char *codegen_generate_champ_code_size(int code_size)
+{
+	char *result;
+
+	result = ft_itoa(code_size);
+	if (!result)
+		return (NULL);
+	return (result);
+}
+
+char *codegen_generate_champ_comment_null(const char *comment)
+{
+	char *result;
+
+	if (!(result = ft_memalloc(COMMENT_LENGTH + 4)))
+	{
+		return (NULL);
+	}
+	ft_memcpy(result, comment, sizeof(comment));
+	return (result);
 }
 
 void codegen(t_codegen *code)
--- codegen/codegen_prototype.h
+++ codegen/codegen_prototype.h
@@ -13,4 +13,24 @@
 #define CG_STAGE_GETLABEL 2
 #define CG_STAGE_UNKNOWN 99
 
+typedef enum			e_commands
+{
+	COM_LIVE = 1,
+	COM_LD,
+	COM_ST,
+	COM_ADD,
+	COM_SUB,
+	COM_AND,
+	COM_OR,
+	COM_XOR,
+	COM_ZJMP,
+	COM_LDI,
+	COM_STI,
+	COM_FORK,
+	COM_LLD,
+	COM_LLDI,
+	COM_LFORK,
+	COM_AFF
+}						t_commands;
+
 #endif //CODEGEN_PROTOTYPE_H
