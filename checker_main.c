156b2cdf0d38d05f6015a6df174287c7a293580f
Clement Missy <cmissy@at-e2.21-school.ru>
2020-02-26 15:53:49 +0300

    pull from working_flow

---
+++ checker_main.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	init_map(t_hash_map *map)
+{
+	
+}
+
+int		main(int c, char **v)
+{
+	t_hash_map		map;
+
+	init_map(&map);
+	// label_checker_put_to_map();
+
+	return (0)
+}
