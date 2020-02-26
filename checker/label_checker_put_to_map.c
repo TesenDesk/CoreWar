3d204c1bac635d28ff50ce90333e9220efde2784
ftothmur <ftothmur@student.21-school.ru>
2020-02-26 15:45:32 +0300

    put_to_map function has bigun.

---
+++ checker/label_checker_put_to_map.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   label_checker_put_to_map.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/26 15:33:48 by ftothmur          #+#    #+#             */
+/*   Updated: 2020/02/26 15:44:35 by ftothmur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "label_checker.h"
+#include "label_checker_private.h"
+
+int				label_checker_put_to_map(t_hash_map **map, )
