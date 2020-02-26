0b27b04186b0daeaf4e27ee7b7c3c340860ed1c8
ftothmur <ftothmur@student.21-school.ru>
2020-02-26 18:58:20 +0300

    hash_table_ func-s now use macros to return instead of concrete value. I added headers. 2 func-s that put label_word names and label_ptr names are implemented.

---
+++ checker/label_checker.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   label_checker.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/26 16:18:04 by ftothmur          #+#    #+#             */
+/*   Updated: 2020/02/26 18:45:28 by ftothmur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LABEL_CHECKER_H
+# define LABEL_CHECKER_H
+
+#include "libft.h"
+
+int				label_checker_put_to_map_label_word(
+				t_hash_map **map_of_label_words, char const **text);
+int				label_checker_put_to_map_label_ptr(
+				t_hash_map **map_of_label_ptrs, char const **text);
+
+#endif
--- checker/label_checker_put_to_map.c
+++ checker/label_checker_put_to_map.c
@@ -6,11 +6,64 @@
 /*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 15:33:48 by ftothmur          #+#    #+#             */
-/*   Updated: 2020/02/26 15:44:35 by ftothmur         ###   ########.fr       */
+/*   Updated: 2020/02/26 18:53:33 by ftothmur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "label_checker.h"
 #include "label_checker_private.h"
 
-int				label_checker_put_to_map(t_hash_map **map, )
+static int		deallocate_pair_key_return_failure(t_pair *pair)
+{
+	free(pair->key);
+	return (FAILURE);
+}
+
+/*
+**	The function adds a unique "label word" name to the assiciative array.
+**	INPUT:	ptr to the associative array, ptr to text where "label word" name
+** begings. 
+**	OUTPUT:	did the operation succeed.
+*/
+
+int				label_checker_put_to_map_label_word(
+				t_hash_map **map_of_label_words, char const **text)
+{
+	size_t		label_len;
+	t_pair		pair;
+
+	label_len = ft_strchr(*text, LABEL_CHAR) - *text;
+	if (!(pair.key = ft_strsub(*text, *text, label_len - 1)))
+		return (FAILURE);
+	pair.content = NULL;
+	if (hm_find(*map_of_label_words, pair.key))
+		return (deallocate_pair_key_return_failure(&pair));
+	if (hm_put(map_of_label_words, pair.key, pair.content) == FAILURE)
+		return (deallocate_pair_key_return_failure(&pair));
+	free(pair.key);
+	return (SUCCESS);
+}
+
+/*
+**	The function adds "label ptr" to the assiciative array if name of the
+** "label ptr" have not been seen before, otherwize do notnthing.
+**	INPUT:	ptr to the associative array, ptr to text where "label word" name
+** begings. 
+**	OUTPUT:	did the operation succeed.
+*/
+
+int				label_checker_put_to_map_label_ptr(
+				t_hash_map **map_of_label_ptrs, char const **text)
+{
+	size_t		label_len;
+	t_pair		pair;
+
+	label_len = ft_strchr(*text, WHITE_SPACE) - *text;
+	if (!(pair.key = ft_strsub(*text, *text, label_len - 1)))
+		return (FAILURE);
+	pair.content = NULL;
+	if (hm_find(*map_of_label_ptrs, pair.key) == NULL)
+		if (hm_put(map_of_label_ptrs, pair.key, pair.content) == FAILURE)
+			return (deallocate_pair_key_return_failure(&pair));
+	free(pair.key);
+	return (SUCCESS);
+}
