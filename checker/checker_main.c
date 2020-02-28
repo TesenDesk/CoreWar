3f9a05dd71bf8f44cc95835cf3c8429716774f4d
Clement Missy <cmissy@at-e1.21-school.ru>
2020-02-28 15:30:48 +0300

    s

--- checker/checker_main.c
+++ checker/checker_main.c
@@ -12,24 +12,30 @@ void		print_hashmap(t_hash_map *map)
 	size_t		i;
 
 	i = 0;
-	while (i < map->map_size)
-	{
-		if (map->data[i] != NULL)
-		{
-			ft_keystr_avl_tree_traversal(map->data[i], print_vert);
-		}
-	}
+	// while (i < map->map_size)
+	// {
+	// 	if (map->data[i] != NULL)
+	// 	{
+	// 		ft_keystr_avl_tree_traversal(map->data[i], print_vert);
+	// 	}
+	// }
 }
 
 int			main(int c, char **v)
 {
-	t_hash_map		*map;
+	t_hash_map		*map_lw;
+	t_hash_map		*map_lp;
+	t_vector		vector;
 	int				i;
 
-	map = create_hashmap(HASH_CONST);
+	map_lw = create_hashmap(HASH_CONST);
+	map_lp = create_hashmap(HASH_CONST);
 	i = 1;
 	while (i < c)
-		label_checker_put_to_map(&map, v[i]);
+	{
+		label_checker_put_to_map(&map, v[i], ft_strlen(v[i]));
+		label_checker_put_to_map(&map, v[i], ft_strlen(v[i]));
+	}
 	print_hashmap(map);
 	return (0);
 }
