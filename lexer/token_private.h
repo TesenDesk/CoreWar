bbb3cd2eff1146ef3f3aeb5f24061bea5168d43e
Clement Missy <cmissy@at-f6.21-school.ru>
2020-03-11 13:46:26 +0300

    f

--- lexer/token_private.h
+++ lexer/token_private.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_private.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmissy <cmissy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 17:44:55 by ftothmur          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/03/05 20:34:00 by cmissy           ###   ########.fr       */
-=======
-/*   Updated: 2020/03/05 21:22:19 by ftothmur         ###   ########.fr       */
->>>>>>> 09a844fd5c990e262fc840d5c787b248f88ce6d1
+/*   Updated: 2020/03/09 17:36:55 by cmissy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +18,6 @@
 
 typedef struct		s_token
 {
-<<<<<<< HEAD
-    int             type;
-    void			*val; // ?????????
-    void		    *token_ptr[2];
-    int             (*get_type)(t_token  *token);
-    void*           (*get_begin)(t_token *token);
-    void*			(*get_val)(t_token *token);
-}				    t_token;
-=======
 	int				type;
 	void			*val;
 	void			*token_ptr[2];
@@ -38,7 +25,6 @@ typedef struct		s_token
 	void			*(*get_begin)(t_token *token);
 	void			*(*get_val)(t_token *token);
 }					t_token;
->>>>>>> 09a844fd5c990e262fc840d5c787b248f88ce6d1
 
 
 #endif
