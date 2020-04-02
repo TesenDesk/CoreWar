f18fda78a5bab80c82475096fcc08270e4dc7089
YurezZ <zapisator@logan-club.ru>
2020-04-02 21:09:44 +0300

    +headers to c-files. +struct to errors and carriage

--- virtual_machine/src/errors/errors_private.h
+++ virtual_machine/src/errors/errors_private.h
@@ -6,11 +6,22 @@
 /*   By: yurezz <yurezz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 17:10:49 by yurezz            #+#    #+#             */
-/*   Updated: 2020/04/01 17:15:43 by yurezz           ###   ########.fr       */
+/*   Updated: 2020/04/02 21:06:44 by yurezz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_PRIVATE_H
 # define ERRORS_PRIVATE_H
+
+enum					e_error_id
+{
+	ENOMEMORY,
+};
+
+typedef struct		s_errors
+{
+
+}					t_errors;
+
 
 #endif
