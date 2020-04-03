fdf83f72e3ab4b7caa69ea4b0f8f15290d199d61
NktTrpv <nikita_toropov@nkt.local>
2020-04-03 18:26:39 +0300

    lexer singlton changed
    Former-commit-id: 9efd6335b6b41ee8c84aecc6b53abb19b0a4609b

--- lexer/lexer_singleton_instance.c
+++ lexer/lexer_singleton_instance.c
@@ -6,7 +6,7 @@
 /*   By: nikita_toropov <nikita_toropov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 17:13:41 by yurezz            #+#    #+#             */
-/*   Updated: 2020/03/20 21:50:58 by nikita_toro      ###   ########.fr       */
+/*   Updated: 2020/04/03 18:26:18 by nikita_toro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ t_lexer				*lexer_singleton_instance(int instantiation_status)
 
 	if (instantiation_status == LEXER_INSTANTIATE)
 	{
-		lexer_instance = NULL;
 		if (!lexer_instance)
 			_lexer_ctor(&lexer_instance); // обращение к lexer_instance до инициализации
 	}
