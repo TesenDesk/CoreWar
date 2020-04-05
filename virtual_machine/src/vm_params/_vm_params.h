53536237bbfb1846e5912b4e82b4d6913ec45265
YurezZ <zapisator@logan-club.ru>
2020-04-06 00:40:21 +0300

    vm_params class is implemented.

--- virtual_machine/src/vm_params/_vm_params.h
+++ virtual_machine/src/vm_params/_vm_params.h
@@ -6,7 +6,7 @@
 /*   By: yurezz <yurezz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 17:15:50 by yurezz            #+#    #+#             */
-/*   Updated: 2020/04/05 20:30:16 by yurezz           ###   ########.fr       */
+/*   Updated: 2020/04/05 21:16:30 by yurezz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@
 typedef struct s_player			t_player;
 typedef struct s_vmp_state		t_vmp_state;
 typedef struct s_parsed_player	t_parsed_player;
+
+typedef (*t_sm_parser)(t_vm_params *self, char **params);
 
 typedef struct					s_vm_params
 {
--- virtual_machine/src/vm_params/vmp_state/vmp_state.c
+++ virtual_machine/src/vm_params/vmp_state/vmp_state.c
@@ -6,36 +6,48 @@
 /*   By: yurezz <yurezz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/02 21:07:27 by yurezz            #+#    #+#             */
-/*   Updated: 2020/04/05 11:48:07 by yurezz           ###   ########.fr       */
+/*   Updated: 2020/04/06 00:21:36 by yurezz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_vmp_state.h"
 
+static int			_vmp_state_due_to_param(char *param)
+{
+	int				state;
+
+	if (param == NULL)
+		state = VMP_STOP;
+	else if (ft_strequ(P_SHORT_DUMP, param) || ft_strequ(P_LONG_DUMP, param))
+		state = IS_DUMP;
+	else if (ft_strequ(P_SHORT_NAME, param) || ft_strequ(P_LONG_NAME, param))
+		state = SET_PLAYER_NAME;
+	else
+		state = SET_FILE_NAME_WITHOUT_ID;
+	return (state);
+}
+
+static int			_vmp_state_due_to_state(int state)
+{
+	if (state == IS_DUMP)
+		state = SET_NBR_CYCLES;
+	else if (state == SET_PLAYER_NAME)
+		state = SET_FILE_NAME_WITH_ID;
+	else
+		state = VMP_INITIAL;
+	return (state);
+}
+
 int					vmp_state(char *param)
 {
 	static int		state;
 
-	if (param == NULL)
-	{
-		if (SET_NBR_CYCLES <= state && state <= SET_FILE_NAME)
-			raise(__FILE__, __LINE__, ENOARGVAL);
-		else
-			state = VMP_STOP;
-	}
-	else if (SET_NBR_CYCLES <= state && state <= SET_FILE_NAME)
-		state = state == SET_PLAYER_NAME ? SET_FILE_NAME : VMP_INITIAL;
+	if (state != VMP_INITIAL && param == NULL)
+		raise(__FILE__, __LINE__, ENOARGVAL);
+	if (state == VMP_INITIAL)
+		state = _vmp_state_due_to_param(param);
 	else
-	{
-		if (
-			ft_strequ(P_SHORT_DUMP, param) || ft_strequ(P_LONG_DUMP, param))
-			state = SET_NBR_CYCLES;
-		else if (
-			ft_strequ(P_SHORT_NAME, param) || ft_strequ(P_LONG_NAME, param))
-			state = SET_PLAYER_NAME;
-		else
-			state = SET_FILE_NAME;
-	}
+		state = _vmp_state_due_to_state(state);
 	return (state);
 }
 
