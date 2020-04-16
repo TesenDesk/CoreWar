e1a6a04ae429750f6d0ede9d1c2c7bb81eb3a59c
Nara_Indra <pindak.andrey@gmail.com>
2020-04-16 21:45:36 +0300

    parser_get_token_init to be fixed
    Former-commit-id: a9e40c39f3d4855e68524263363241bf90f1a9bf

--- parser/_parser_get_token_init.c
+++ parser/_parser_get_token_init.c
@@ -10,18 +10,19 @@ static void		fill_expr_types(int expr_types[TOKEN_LABEL_WORD + 2])
 
 	index_token = TOKEN_LFEED;
 	value_expr = EXPR_LFEED;
-	while (index_token <= TOKEN_STI && value_expr <= EXPR_OP_STRI)
-		expr_types[index_token++] = value_expr++;
-	while (index_token <= TOKEN_LLDI && value_expr <= EXPR_OP_LODI)
-	{
-		expr_types[index_token++] = value_expr;
-		expr_types[index_token++] = value_expr++;
-	}
-	while (index_token <= TOKEN_XOR && value_expr == EXPR_OP_LOGC)
-		expr_types[index_token++] = value_expr;
-	value_expr++;
-	while (index_token <= TOKEN_LFORK && value_expr == EXPR_OP_LIFE)
-		expr_types[index_token++] = value_expr;
+//	expr[0]
+//	while (index_token <= TOKEN_STI && value_expr <= EXPR_OP_STRI)
+//		expr_types[index_token++] = value_expr++;
+//	while (index_token <= TOKEN_LLDI && value_expr <= EXPR_OP_LODI)
+//	{
+//		expr_types[index_token++] = value_expr;
+//		expr_types[index_token++] = value_expr++;
+//	}
+//	while (index_token <= TOKEN_XOR && value_expr == EXPR_OP_LOGC)
+//		expr_types[index_token++] = value_expr;
+//	value_expr++;
+//	while (index_token <= TOKEN_LFORK && value_expr == EXPR_OP_LIFE)
+//		expr_types[index_token++] = value_expr;
 }
 
 int				_parser_get_token_init(t_parser *parser, t_lexer *lexer,
@@ -29,17 +30,18 @@ int				_parser_get_token_init(t_parser *parser, t_lexer *lexer,
 {
 	t_token		*token;
 	int			token_type;
-	static int	expr_types[TOKEN_LFORK + 1];
+//	static int	expr_types[TOKEN_LFORK + 1];
 
 	if ((token = lexer_form_token(lexer, text)) == NULL)
 		return (NO_TOKEN);
 	token_type = token_get_type(token);
-	if (!expr_types[TOKEN_LFORK])
-		fill_expr_types(expr_types);
-	else if (token_type >= TOKEN_LFEED && token_type <= TOKEN_LFORK)
-		expr->type = expr_types[token_type];
-	else
-		expr->type = EXPR_UNDEF;
+//	if (token_type >=)
+//	if (!expr_types[TOKEN_LFORK])
+//		fill_expr_types(expr_types);
+//	else if (token_type >= TOKEN_LFEED && token_type <= TOKEN_LFORK)
+//		expr->type = expr_types[token_type];
+//	else
+//		expr->type = EXPR_UNDEF;
 	if (expr_set_arg(expr, token, OP_NAME, token_type) == FAILURE)
 		return (NO_TOKEN);
 	return (token_type);
