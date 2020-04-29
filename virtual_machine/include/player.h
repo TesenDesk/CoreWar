2a1c7b1a30ebb028f53202a4d5e65f9553cad103
NktTrpv <nikita_toropov@nkt.local>
2020-04-29 18:31:00 +0300

    turn back player_set_name
    Former-commit-id: bee14d372ec03aed19ff2e332d012a3ac927b6d2

--- virtual_machine/include/player.h
+++ virtual_machine/include/player.h
@@ -6,7 +6,7 @@
 /*   By: nikita_toropov <nikita_toropov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 17:10:07 by yurezz            #+#    #+#             */
-/*   Updated: 2020/04/26 21:44:10 by nikita_toro      ###   ########.fr       */
+/*   Updated: 2020/04/29 18:29:50 by nikita_toro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 typedef struct s_player	t_player;
 
 // t_player		*player_new(t_file *file);
-// void		player_set_name(t_player *self, int name);
 
+void			player_set_name(t_player *self, int name);
 int				player_name(t_player *self);
 t_player		*player_new(t_vmp_player *vmp_player);
 void			player_destroy(t_player **self);
