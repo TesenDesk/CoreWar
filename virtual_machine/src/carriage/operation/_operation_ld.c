e6083485288501f17e2017ad9fbf18a0e9411bd3
NktTrpv <nikita_toropov@nkt.local>
2020-06-15 16:39:23 +0300

    new changes in _operation_ld
    Former-commit-id: f65b01f8313d29908fd5f0f8c62b6ba53a22e859

--- virtual_machine/src/carriage/operation/_operation_ld.c
+++ virtual_machine/src/carriage/operation/_operation_ld.c
@@ -10,14 +10,16 @@ int                 t_dir_size_for_the_op(int op_code)
 // fill args, type_codes and op_len
 // return SUCCESS all args_codes is real
 // else return FAILURE
-int                    precheck_args(t_carriage *self, int *args, char *type_codes, int num_of_args, int *op_len) {
+int                    precheck_args(t_carriage *self, int *args, char *type_codes, int num_of_args, int *op_len)
+{
     char    argument_type_code;
     int     i;
     int     result;
     int     t_dir_size;
     int     t_ind;
 
-    argument_type_code = (char) arena_get_n_bytes_from(self->arena, self->arena_position + ONE_BYTE, ONE_BYTE);
+    argument_type_code =
+            (char)arena_get_n_bytes_from(self->arena, self->arena_position + ONE_BYTE, ONE_BYTE);
 
     result = SUCCESS;
     *op_len = ONE_BYTE + ONE_BYTE;
@@ -56,53 +58,70 @@ int                    precheck_args(t_carriage *self, int *args, char *type_cod
 
 void                    _operation_ld(t_carriage *self)
 {
-    char     argument_type_code;
-    int      t_ind;
-    int      first_arg;
-    int      second_arg;
-    int      op_len;
+//    need change "2" to constant later
+    int     op_len;
+    int     args[2];
+    char    type_codes[2];
 
-    op_len = ONE_BYTE;
-    argument_type_code =
-            (char) arena_get_n_bytes_from(self->arena, self->arena_position + op_len, ONE_BYTE);
-    if (argument_type_code >> 6 == CODE_T_DIR)
+    if (precheck_args(self, args, type_codes, 2, &op_len) == SUCCESS)
     {
-        first_arg =
-                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, FOUR_BYTES);
-        op_len += FOUR_BYTES;
-    }
-    else if (argument_type_code >> 6 == CODE_T_IND)
-    {
-        t_ind =
-                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, TWO_BYTES) % IDX_MOD;
-        first_arg =
-                arena_get_n_bytes_from(self->arena, self->arena_position + t_ind, FOUR_BYTES);
-        op_len += TWO_BYTES;
+//        if ((self->registers[args[1]] = args[0]) == 0)
+        if ((self->registers[args[ARG_2]] = args[ARG_1]) == 0)
+            self->carry = 1;
+        else
+            self->carry = 0;
     }
     else
-    {
-        self->arena_position += op_len;
-        return;
-    }
-
-    if ((argument_type_code >> 4 & TWO_BITS_MASK) == CODE_T_REG)
-    {
-        second_arg =
-                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, ONE_BYTE);
-        op_len += ONE_BYTE;
-//        check register valid!!! dont know what to do when register isn`t valid
-    }
-    else
-    {
-        self->arena_position += op_len;
-        return;
-    }
+        self->arena_position = (self->arena_position + op_len) % MEM_SIZE;
 
 
-//    perform operation ld
-    if ((self->registers[second_arg] = first_arg) == 0)
-        self->carry = 1;
-    else
-        self->carry = 0;
-    self->arena_position += op_len;
+//    char     argument_type_code;
+//    int      t_ind;
+//    int      first_arg;
+//    int      second_arg;
+//    int      op_len;
+//
+//    op_len = ONE_BYTE;
+//    argument_type_code =
+//            (char) arena_get_n_bytes_from(self->arena, self->arena_position + op_len, ONE_BYTE);
+//    if (argument_type_code >> 6 == CODE_T_DIR)
+//    {
+//        first_arg =
+//                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, FOUR_BYTES);
+//        op_len += FOUR_BYTES;
+//    }
+//    else if (argument_type_code >> 6 == CODE_T_IND)
+//    {
+//        t_ind =
+//                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, TWO_BYTES) % IDX_MOD;
+//        first_arg =
+//                arena_get_n_bytes_from(self->arena, self->arena_position + t_ind, FOUR_BYTES);
+//        op_len += TWO_BYTES;
+//    }
+//    else
+//    {
+//        self->arena_position += op_len;
+//        return;
+//    }
+//
+//    if ((argument_type_code >> 4 & TWO_BITS_MASK) == CODE_T_REG)
+//    {
+//        second_arg =
+//                arena_get_n_bytes_from(self->arena, self->arena_position + op_len, ONE_BYTE);
+//        op_len += ONE_BYTE;
+////        check register valid!!! dont know what to do when register isn`t valid
+//    }
+//    else
+//    {
+//        self->arena_position += op_len;
+//        return;
+//    }
+//
+//
+////    perform operation ld
+//    if ((self->registers[second_arg] = first_arg) == 0)
+//        self->carry = 1;
+//    else
+//        self->carry = 0;
+//    self->arena_position += op_len;
 }
