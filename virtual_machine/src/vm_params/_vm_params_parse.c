b07aa3fe970e93de6680301d351c39c6b24133f7
NaraIndra <pindak.andrey@gmail.com>
2020-06-26 12:08:00 -0400

    silly miss in -v flag
    Former-commit-id: 28849f77d1bf5da59e56258c3c686f408ce993d5

--- virtual_machine/src/vm_params/_vm_params_parse.c
+++ virtual_machine/src/vm_params/_vm_params_parse.c
@@ -174,7 +174,7 @@ static int 				_vm_params_flag_verbose(t_vm_params *self, char *param)
 	num = ft_atol(param);
 	if (!(ft_arg_is_num(param)))
 		raise(__FILE__, __LINE__, ENOARGVAL);
-	self->verbosity_lvl = 2;
+	self->verbosity_lvl = num;
 	return (FLAG_NUM_CODE);
 
 }
