d24a13e9a2edac89faac256aee16a2c8327ba5d5
Clement Missy <cmissy@il-h3.msk.21-school.ru>
2020-07-13 15:14:00 +0300

    fix problem with multyple -n
    Former-commit-id: d75f843b84ba1f6b5402cf130d3eb27cc21404fa

--- virtual_machine/src/corwar/corwar.c
+++ virtual_machine/src/corwar/corwar.c
@@ -17,19 +17,16 @@ static void		_corwar_precondition_check()
 	if (
 		(REG_SIZE < sizeof(char) || sizeof(int) < REG_SIZE) ||
 		IND_SIZE < sizeof(char) ||
-
 		MAX_ARGS_NUMBER < 4 ||
 		MAX_PLAYERS < 1 ||
 		REG_NUMBER < 1 ||
-
 		IDX_MOD < 1 ||
 		DIR_SIZE != REG_SIZE ||
 		CYCLE_DELTA < 1 ||
 		MAX_CHECKS < 1 ||
-
 		NBR_LIVE < 0)
 		raise(__FILE__, __LINE__, EINVALCONSTANTS);
-	return;
+	return ;
 }
 
 int				main(int argc, char *argv[])
--- virtual_machine/src/vm_params/prvt_vm_params_set_file_name_with_id.c
+++ virtual_machine/src/vm_params/prvt_vm_params_set_file_name_with_id.c
@@ -16,8 +16,12 @@ void				prvt_vm_params_set_file_name_with_id(t_vm_params *self,
 							char *file_name)
 {
 	t_vmp_player	*player;
+	t_list 			*lst_vmp_players;
 
-	player = (t_vmp_player *)self->id_player_head->content;
+	lst_vmp_players = self->id_player_head;
+	while (lst_vmp_players && lst_vmp_players->next)
+		lst_vmp_players = lst_vmp_players->next;
+	player = (t_vmp_player *)lst_vmp_players->content;
 	vmp_player_set_file(player, file_name);
 	return ;
 }
