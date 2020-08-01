451a7d5526e139c2a06a5deb2ea6e4c8cb73bb73
Falmer Tothmure <ftothmur@oa-a4.msk.21-school.ru>
2020-08-01 12:34:00 +0300

    Is normalized. The name at define is changed.
    Former-commit-id: 69d107eed5f122ad9ab48a0c971faf7493a772b5

--- virtual_machine/src/visual/prvt_visual.h
+++ virtual_machine/src/visual/prvt_visual.h
@@ -1,34 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prvt_visual.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/01 12:14:40 by ftothmur          #+#    #+#             */
+/*   Updated: 2020/08/01 12:31:29 by ftothmur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef VISUAL_PRIVATE_H
-#define VISUAL_PRIVATE_H
-#include "libft.h"
-#include "visual.h"
+#ifndef PRVT_VISUAL_H
+# define PRVT_VISUAL_H
 
+# include "libft.h"
+# include "visual.h"
+# include "op.h"
 
-#include "op.h"
-#define EXTERNAL_COLOR_OFFSET   4
+# define EXTERNAL_COLOR_OFFSET	4
 # define COLOR_GRAY				8
 
-#define P_1_HOME        1
-#define P_2_HOME        2
-#define P_3_HOME        3
-#define P_4_HOME        4
-#define P_1_L_HOME      5
-#define P_2_L_HOME      6
-#define P_3_L_HOME      7
-#define P_4_L_HOME      8
-#define P_1_L_OUT       9
-#define P_2_L_OUT       10
-#define P_3_L_OUT       11
-#define P_4_L_OUT       12
-#define P_1_L_WIN       13
-#define P_2_L_WIN       14
-#define P_3_L_WIN       15
-#define P_4_L_WIN       16
-#define B_INFO          17
-#define NEUTRAL_COL     18
+# define P_1_HOME				1
+# define P_2_HOME				2
+# define P_3_HOME				3
+# define P_4_HOME				4
+# define P_1_L_HOME				5
+# define P_2_L_HOME				6
+# define P_3_L_HOME				7
+# define P_4_L_HOME				8
+# define P_1_L_OUT				9
+# define P_2_L_OUT				10
+# define P_3_L_OUT				11
+# define P_4_L_OUT				12
+# define P_1_L_WIN				13
+# define P_2_L_WIN				14
+# define P_3_L_WIN				15
+# define P_4_L_WIN				16
+# define B_INFO					17
+# define NEUTRAL_COL			18
 
-#define HOME_OFFSET     4
-#define OUT_OFFSET      8
+# define HOME_OFFSET			4
+# define OUT_OFFSET				8
 
 #endif
