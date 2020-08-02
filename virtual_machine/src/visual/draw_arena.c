f8415e3ef2fd7b6877c8ab73a163ede21edc9401
Falmer Tothmure <ftothmur@mi-l3.msk.21-school.ru>
2020-08-02 13:40:01 +0300

    Replaced proper elements with proper getters at draw_arena().
    Former-commit-id: f65ac7ed762eb2fd75eb427d7bd4904e65bd8ea9

--- virtual_machine/src/visual/draw_arena.c
+++ virtual_machine/src/visual/draw_arena.c
@@ -6,7 +6,7 @@
 /*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 12:49:20 by ftothmur          #+#    #+#             */
-/*   Updated: 2020/08/01 13:17:30 by ftothmur         ###   ########.fr       */
+/*   Updated: 2020/08/02 13:37:44 by ftothmur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,20 @@ void		draw_arena(t_vm *vm)
 
 	i = 0;
 	j = 0;
-	rebuild_color_map(vm->arena, vm);
+	rebuild_color_map(vm_get_arena(vm), vm);
 	while (i < SQRT_MAP)
 	{
-		wmove(vm->wins->arena, i + 1, 1);
+		wmove(vm_get_wins(vm)->arena, i + 1, 1);
 		while (j < SQRT_MAP)
 		{
-			col = chose_color(vm->arena, (i * 64 + j) % MEM_SIZE, vm, i);
-			wattron(vm->wins->arena, col);
-			wprintw(vm->wins->arena, "%.2x",
-				(unsigned char)vm->arena->data[(i * 64 + j++) % MEM_SIZE]);
-			wattroff(vm->wins->arena, col);
-			waddch(vm->wins->arena, ' ');
+			col = chose_color(vm_get_arena(vm), (i * 64 + j) % MEM_SIZE, vm, i);
+			wattron(vm_get_wins(vm)->arena, col);
+			wprintw(vm_get_wins(vm)->arena, "%.2x",
+				(unsigned char)vm_get_arena(vm)->data[(i * 64 + j++) % MEM_SIZE]);
+			wattroff(vm_get_wins(vm)->arena, col);
+			waddch(vm_get_wins(vm)->arena, ' ');
 		}
-		wprintw(vm->wins->arena, "\n");
+		wprintw(vm_get_wins(vm)->arena, "\n");
 		j = 0;
 		++i;
 	}
