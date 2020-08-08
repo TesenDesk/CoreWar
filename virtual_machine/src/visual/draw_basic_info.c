02af0b8895f9b1004b61aca459af90a83a9076ac
Falmer Tothmure <ftothmur@mi-h1.msk.21-school.ru>
2020-08-08 16:26:12 +0300

    Сделал vm_get_...(). Заменил в draw_basic_info() на vm_get_cycles_counter().
    Former-commit-id: 62cbd89186355620ac6f55ddc63da6dd519b77fc

--- virtual_machine/src/visual/draw_basic_info.c
+++ virtual_machine/src/visual/draw_basic_info.c
@@ -6,7 +6,7 @@
 /*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 12:46:53 by ftothmur          #+#    #+#             */
-/*   Updated: 2020/08/08 15:56:24 by ftothmur         ###   ########.fr       */
+/*   Updated: 2020/08/08 16:25:25 by ftothmur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,34 @@
 
 void			draw_basic_info(t_vm *vm)
 {
+	// wattron(vm_get_wins(vm)->info, COLOR_PAIR(B_INFO) | A_BOLD);
+	// wmove(vm_get_wins(vm)->info, 1, 1);
+	// wprintw(vm_get_wins(vm)->info, " CYCLES_TO_DIE:   %zu\n",
+	// 	(unsigned int)vm->cycles_to_die);
+	// wmove(vm_get_wins(vm)->info, 2, 1);
+	// wprintw(vm_get_wins(vm)->info, " CURRENT_COUNTER: %zu\n",
+	// 	(unsigned int)vm->cycles_counter);
+	// wmove(vm_get_wins(vm)->info, 3, 1);
+	// wprintw(vm_get_wins(vm)->info, " GLOBAL_COUNTER:  %d\n", vm->global_counter);
+	// wmove(vm_get_wins(vm)->info, 4, 1);
+	// wprintw(vm_get_wins(vm)->info, " CYCLES_DELTA:    %d\n", CYCLE_DELTA);
+	// wmove(vm_get_wins(vm)->info, 5, 1);
+	// wprintw(vm_get_wins(vm)->info, " GAME_SPEED:      %d\n", vm->speed);
+	// wmove(vm_get_wins(vm)->info, 6, 1);
+	// wprintw(vm_get_wins(vm)->info,
+	// 	" TO SPEED_UP PUSH UP_ARROW, "
+	// 	"TO SPEED_DOWN PUSH DOWN_ARROW(MIN 1 MAX 10)");
+	// wmove(vm_get_wins(vm)->info, 7, 1);
+	// wprintw(vm_get_wins(vm)->info,
+	// 	"                 PRESS 'x' TO GO TO JUMP TO THE END");
+	// wattroff(vm_get_wins(vm)->info, COLOR_PAIR(B_INFO) | A_BOLD);
 	wattron(vm_get_wins(vm)->info, COLOR_PAIR(B_INFO) | A_BOLD);
 	wmove(vm_get_wins(vm)->info, 1, 1);
 	wprintw(vm_get_wins(vm)->info, " CYCLES_TO_DIE:   %zu\n",
 		(unsigned int)vm->cycles_to_die);
 	wmove(vm_get_wins(vm)->info, 2, 1);
 	wprintw(vm_get_wins(vm)->info, " CURRENT_COUNTER: %zu\n",
-		(unsigned int)vm->cycles_counter);
+		vm_get_cycles_counter(vm));
 	wmove(vm_get_wins(vm)->info, 3, 1);
 	wprintw(vm_get_wins(vm)->info, " GLOBAL_COUNTER:  %d\n", vm->global_counter);
 	wmove(vm_get_wins(vm)->info, 4, 1);
