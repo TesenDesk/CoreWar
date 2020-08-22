6b503448380cf152e3cc01fd6200b067d2f92b74
Falmer Tothmure <ftothmur@am-k1.msk.21-school.ru>
2020-08-22 13:14:12 +0300

    fix 4
    Former-commit-id: a3d8de6edd8eb302ec27f64372ec7cf2b22e7972

--- asm_machine/parser/expr_set_size.c
+++ asm_machine/parser/expr_set_size.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expr_set_size.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmissy <cmissy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ftothmur <ftothmur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 16:36:09 by cmissy            #+#    #+#             */
-/*   Updated: 2020/07/19 16:44:01 by cmissy           ###   ########.fr       */
+/*   Updated: 2020/08/22 13:13:42 by ftothmur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void		expr_set_size(t_expr *q)
 {
 	if (q->args[OP_NAME].type == TOKEN_ZJMP
-		|| q->args[OP_NAME].type == TOKEN_LDI || q->args[OP_NAME].type == TOKEN_STI
+		|| q->args[OP_NAME].type == TOKEN_LDI
+		|| q->args[OP_NAME].type == TOKEN_STI
 		|| q->args[OP_NAME].type == TOKEN_FORK
 		|| q->args[OP_NAME].type == TOKEN_LLDI
 		|| q->args[OP_NAME].type == TOKEN_LFORK)
