d8c6edab7cf18d6b9de71258c3c9a885986906e3
Mudcrab Stygg <mstygg@mi-n5.msk.21-school.ru>
2020-08-29 22:17:01 +0300

    sega in draw_champ_info found and annihilated
    Former-commit-id: f86a613a752c1ca2f8b2b8a09db73d08e158ad9f

--- virtual_machine/src/visual/draw_champ_info.c
+++ virtual_machine/src/visual/draw_champ_info.c
@@ -15,7 +15,7 @@
 void		draw_champ_info(t_vm *vm)
 {
 	int		i;
-	char	*tmp;
+//	char	*tmp;
 	t_wins	*wins;
 
 	i = 0;
@@ -25,13 +25,17 @@ void		draw_champ_info(t_vm *vm)
 		wattron(wins->champ, COLOR_PAIR(P_1_HOME + i) | A_BOLD);
 		wmove(wins->champ, i * 3 + 1, 1);
 		wprintw(wins->champ, "P_%d_name: %62s", i + 1,
-			(tmp = player_get_text_name(arena_get_player(vm_get_arena(vm),
-												i))));
-		ft_memdel((void **)&tmp);
+			player_get_text_name(arena_get_player(vm_get_arena(vm),
+												i)));
+
+//        wprintw(wins->champ, "P_%d_name: %62s", i + 1,
+//        tmp =        player_get_text_name(arena_get_player(vm_get_arena(vm),
+//                                                      i)));
+//		ft_memdel((void **)&tmp);
 		wmove(wins->champ, i * 3 + 2, 1);
 		wprintw(wins->champ, "P_%d_comment: %59s", i + 1,
 			player_get_text_comment(arena_get_player(vm_get_arena(vm), i)));
-		ft_memdel((void **)&tmp);
+//		ft_memdel((void **)&tmp);
 		wmove(wins->champ, i * 3 + 3, 1);
 		wprintw(wins->champ, "carriage_num: %58d",
 			arena_get_carriage_num(vm_get_arena(vm), i));
