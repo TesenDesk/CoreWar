2bf6c9be4f607d4aead0d5f3b658019245d10656
Mudcrab Stygg <mstygg@mi-m5.msk.21-school.ru>
2020-08-30 18:50:13 +0300

    lexer_get_term_init cut and checked
    Former-commit-id: fef8876e0cad918bc30d95b5df4eb139ddfaa348

--- asm_machine/lexer/lexer_get_term_init.c
+++ asm_machine/lexer/lexer_get_term_init.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "prvt_lexer.h" //TODO: Norme error: Too many functions in the file
+#include "prvt_lexer.h"
 
 static int		term_is_op(char const *text)
 {
@@ -113,80 +113,109 @@ int				lexer_check_eof(int *type, char const **text, void *token_ptr[2])
 	return (COMMENT_CHAR_CODE);
 }
 
+
+int         lexer_get_term_init_header(char **text)
+{
+    if (ft_strnstr(*text, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)))
+    {
+        if (*(*text + ft_strlen(NAME_CMD_STRING)) == ' '
+            || *(*text + ft_strlen(NAME_CMD_STRING)) == '\t')
+        {
+            (*text) += ft_strlen(NAME_CMD_STRING);
+            return (NAME_CMD_STRING_CODE);
+        }
+    }
+    else if (ft_strnstr(*text, COMMENT_CMD_STRING,
+                          ft_strlen(COMMENT_CMD_STRING)))
+    {
+        if (*(*text + ft_strlen(COMMENT_CMD_STRING)) == ' '
+            || *(*text + ft_strlen(COMMENT_CMD_STRING)) == '\t')
+        {
+            (*text) += ft_strlen(COMMENT_CMD_STRING);
+            return (COMMENT_CMD_STRING_CODE);
+        }
+
+    }
+    return (-2);
+}
+
+int            lexer_get_term_little_chars(int *type, char **text, void *token_ptr[2])
+{
+    if (!(**text))
+    {
+        *type = TOKEN_EOF;
+        token_ptr[0] = (void*)*text;
+        token_ptr[1] = (void*)*text;
+        return (EOF_CODE);
+    }
+    else if (**text == COMMENT_CHAR)
+    {
+        ++(*text);
+        return (COMMENT_CHAR_CODE);
+    }
+    else if (**text == ALT_COMMENT_CHAR)
+    {
+        ++(*text);
+        return (ALT_COMMENT_CHAR_CODE);
+    }
+    else if (**text == LINE_FEED)
+    {
+        *type = TOKEN_LFEED;
+        token_ptr[0] = (void*)*text;
+        token_ptr[1] = (void*)*text;
+        ++(*text);
+        return (INIT_ST);
+    }
+   return (-2);
+}
+
+int             lexer_get_term_op_label_eof(char **text, int *type, void *token_ptr[2])
+{
+    int op_len;
+
+    op_len = 0;
+    if ((op_len = term_is_op(*text)))
+    {
+        *type = find_op_type(*text);
+        token_ptr[0] = (void*)(*text);
+        token_ptr[1] = (void*)(*text + op_len);
+        *text += op_len;
+        return (OPX_CODE);
+    }
+    else if (strchr(LABEL_CHARS, **text))
+    {
+        *token_ptr = (void*)(*text);
+        while (strchr(LABEL_CHARS, *(*text)))
+            ++(*text);
+        *(token_ptr + 1) = (void*)(*text - 1);
+        return (LABEL_CHARS_CODE);
+    }
+    else if (**text == TOKEN_EOF)
+    {
+        *type = TOKEN_EOF;
+        return (EOF_CODE);
+    }
+    return (-2);
+}
+
+
+
+
 int				lexer_get_term_init(t_lexer *lexer, char const **text,
 									int *type, void *token_ptr[2])
 {
 	int			op_len;
-//TODO: Norme error: function too big
+	int         stupid_res;
+
+	stupid_res = 0;
 	while (**text == ' ' || **text == '\t')
 		++(*text);
-//	if (lexer_check_eof(type, text, token_ptr) == EOF_CODE)
-//		return EOF_CODE;
-	if (!(**text))
-	{
-		*type = TOKEN_EOF;
-		token_ptr[0] = (void*)*text;
-		token_ptr[1] = (void*)*text;
-		return (EOF_CODE);
-	}
-	else if (**text == COMMENT_CHAR)
-	{
-		++(*text);
-		return (COMMENT_CHAR_CODE);
-	}
-	else if (**text == ALT_COMMENT_CHAR)
-	{
-		++(*text);
-		return (ALT_COMMENT_CHAR_CODE);
-	}
-	else if (**text == LINE_FEED)
-	{
-		*type = TOKEN_LFEED;
-		token_ptr[0] = (void*)*text;
-		token_ptr[1] = (void*)*text;
-		++(*text);
-		return (INIT_ST);
-	}
-	else if (ft_strnstr(*text, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)))
-	{
-		if (*(*text + ft_strlen(NAME_CMD_STRING)) == ' '
-		|| *(*text + ft_strlen(NAME_CMD_STRING)) == '\t')
-		{
-			(*text) += ft_strlen(NAME_CMD_STRING);
-			return (NAME_CMD_STRING_CODE);
-		}
-	}
-	else if (ft_strnstr(*text, COMMENT_CMD_STRING,
-	ft_strlen(COMMENT_CMD_STRING)))
-	{
-		if (*(*text + ft_strlen(COMMENT_CMD_STRING)) == ' '
-		|| *(*text + ft_strlen(COMMENT_CMD_STRING)) == '\t')
-		{
-			(*text) += ft_strlen(COMMENT_CMD_STRING);
-			return (COMMENT_CMD_STRING_CODE);
-		}
-	}
-	else if ((op_len = term_is_op(*text)))
-	{
-		*type = find_op_type(*text);
-		token_ptr[0] = (void*)(*text);
-		token_ptr[1] = (void*)(*text + op_len);
-		*text += op_len;
-		return (OPX_CODE);
-	}
-	else if (strchr(LABEL_CHARS, **text))
-	{
-		*token_ptr = (void*)(*text);
-		while (strchr(LABEL_CHARS, *(*text)))
-			++(*text);
-		*(token_ptr + 1) = (void*)(*text - 1);
-		return (LABEL_CHARS_CODE);
-	}
-	else if (**text == TOKEN_EOF)
-	{
-		*type = TOKEN_EOF;
-		return (EOF_CODE);
-	}
+    if ((stupid_res = lexer_get_term_little_chars(type,text, token_ptr)) != -2)
+        return (stupid_res);
+	if ((stupid_res = lexer_get_term_init_header(text)) != -2)
+	    return (stupid_res);
+	if ((stupid_res = lexer_get_term_op_label_eof(text, type, token_ptr))  != -2)
+		return (stupid_res);
 	*type = (TOKEN_UNDEF);
 	++(*text);
 	return (TERM_UNDEFINED_CODE);
